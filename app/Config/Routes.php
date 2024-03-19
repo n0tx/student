e606cda0b798897d86b5693a6f021f88a32a03d8
n0tx <rcandra91@msn.com>
2024-03-19 07:20:18 +0700

    - create student model create student controller, create student routes, create student view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Student Crud Routes
+$routes->get('student', 'StudentController::index');
+$routes->post('student/store', 'StudentController::store');
+$routes->get('student/edit/(:num)', 'StudentController::edit/$1');
+$routes->get('student/delete/(:num)', 'StudentController::delete/$1');
+$routes->post('student/update', 'StudentController::update');
---
+++ app/Controllers/StudentController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\StudentModel;
+
+class StudentController extends BaseController
+{
+    public function index()
+    {    
+        $model = new StudentModel();
+   
+        $data['students_detail'] = $model->orderBy('id', 'DESC')->findAll();
+          
+        return view('student_list', $data);
+    }    
+  
+   
+    public function store()
+    {  
+        helper(['form', 'url']);
+           
+        $model = new StudentModel();
+          
+        $data = [
+            'name' => $this->request->getVar('txtName'),
+            'address'  => $this->request->getVar('txtAddress'),
+            ];
+        $save = $model->insert_data($data);
+        if($save != false)
+        {
+            $data = $model->where('id', $save)->first();
+            echo json_encode(array("status" => true , 'data' => $data));
+        }
+        else{
+            echo json_encode(array("status" => false , 'data' => $data));
+        }
+    }
+   
+    public function edit($id = null)
+    {
+        
+     $model = new StudentModel();
+      
+     $data = $model->where('id', $id)->first();
+       
+    if($data){
+            echo json_encode(array("status" => true , 'data' => $data));
+        }else{
+            echo json_encode(array("status" => false));
+        }
+    }
+   
+    public function update()
+    {  
+   
+        helper(['form', 'url']);
+           
+        $model = new StudentModel();
+  
+        $id = $this->request->getVar('hdnStudentId');
+  
+         $data = [
+            'name' => $this->request->getVar('txtName'),
+            'address'  => $this->request->getVar('txtAddress'),
+            ];
+  
+        $update = $model->update($id,$data);
+        if($update != false)
+        {
+            $data = $model->where('id', $id)->first();
+            echo json_encode(array("status" => true , 'data' => $data));
+        }
+        else{
+            echo json_encode(array("status" => false , 'data' => $data));
+        }
+    }
+   
+    public function delete($id = null){
+        $model = new StudentModel();
+        $delete = $model->where('id', $id)->delete();
+        if($delete)
+        {
+           echo json_encode(array("status" => true));
+        }else{
+           echo json_encode(array("status" => false));
+        }
+    }
+}
